f6c08dfe101ddfdf2df3e1f657305517f266a309
parosh <124004074@sastra.ac.in>
2024-05-20 16:24:49 +0530

    9th commit

--- virtual_shop/wsgi.py
+++ virtual_shop/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import nltk
 
 from django.core.wsgi import get_wsgi_application
+
+nltk.download('punkt')
+nltk.download('wordnet')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'virtual_shop.settings')
 
